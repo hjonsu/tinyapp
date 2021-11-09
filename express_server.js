b2fbec9c3020bd25d0da182c866e3a8ac6255915
Jonathan Su <jsu6898@gmail.com>
2021-11-08 21:20:39 -0800

    added randomChar function

--- express_server.js
+++ express_server.js
@@ -4,12 +4,18 @@ const PORT = 8080; // default port 8080
 
 app.set("view engine", "ejs");
 
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
 
-app.get("/urls", (req, res) => {
-  const templateVars = {
-    urls: urlDatabase
-  };
-  res.render("urls_index", templateVars);
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.get("/hello", (req, res) => {
@@ -19,13 +25,15 @@ app.get("/hello", (req, res) => {
   res.render("url_index", templateVars);
 });
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+app.get("/urls", (req, res) => {
+  const templateVars = {
+    urls: urlDatabase
+  };
+  res.render("urls_index", templateVars);
+});
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
@@ -36,6 +44,23 @@ app.get("/urls/:shortURL", (req, res) => {
 
   res.render("urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
+const generateRandomString = () => {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() *
+      charactersLength));
+  }
+  return result;
+};
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
